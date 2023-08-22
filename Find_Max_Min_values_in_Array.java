04111ff8af9cf67dc9827a0c4acd1081107a7c47
darshdec96 <107927160+darshdec96@users.noreply.github.com>
2023-08-22 10:32:25 +0530

    Update Find_Max_Min_values_in_Array.java

--- Find_Max_Min_values_in_Array.java
+++ Find_Max_Min_values_in_Array.java
@@ -5,7 +5,7 @@ public class Find_Max_Min_values_in_Array {
 	public static void main(String[] args) {
 		int a[]= {50,100,40,20,60};
 		int max=a[0];
-		for(int i=1; i<a.length; i++) {
+		for(int i=0; i<a.length; i++) {
 			if(a[i]>max) {
 				max=a[i];
 			}
@@ -13,7 +13,7 @@ public class Find_Max_Min_values_in_Array {
 		System.out.println("Max Elements in Array : "+max);
 		
 		int min=a[0];
-		for(int i=1; i<a.length; i++) {
+		for(int i=0; i<a.length; i++) {
 			if(a[i]<min) {
 				min=a[i];
 			}
